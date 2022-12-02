088c8155fc7f56b917e722dbdd062c5f478f7fca
unknown <andrey.detochkin@gmail.com>
2022-12-02 15:30:54 +0100

    design + new functions

--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 ```
 Description
 
+Note: The project will be updated with new features later.
+
 This is a part of the templates project
 which is containing different parts of the 
 web projects, that can be combined together
@@ -16,7 +18,8 @@ in the more big projects.
 Using API movie database to render the results with
 possibility to filter content by genre.
 
-
-
+The result changes are animated.
 
 ```
+
+#### Andrejs Grocs 2022
--- src/movie.js
+++ src/movie.js
@@ -3,13 +3,15 @@ import {motion} from 'framer-motion'
 
 function Movie({movie}) {
   return (
-    <motion.div 
+    <motion.div className='card'
         animate={{opacity:1}} 
         initial={{opacity: 0}} 
         exit={{opacity:0}}
         layout>
-        <h2>{movie.title}</h2>
+        <h2 className='movie-title'>{movie.title}</h2>
+        
         <img src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} alt=''/>
+        <h2 className='movie-release-date'> Release Date {movie.release_date}</h2>
 
     </motion.div>
   )
